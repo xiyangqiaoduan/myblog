ae4b8b9a3bd686e23c18b28fc028d12b1ccc2ccb
xiyangqiaoduan <xiyangqiaoduan08@163.com>
2017-05-15 07:32:40 +0800

    欢迎

--- src/main/java/com/ycb/blog/web/controller/admin/IndexAdminController.java
+++ src/main/java/com/ycb/blog/web/controller/admin/IndexAdminController.java
@@ -2,7 +2,10 @@ package com.ycb.blog.web.controller.admin;
 
 import com.ycb.blog.common.util.base.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 /**
  * ${DESCRIPTION}
@@ -18,6 +21,20 @@ public class IndexAdminController extends BaseController {
     public String index(){
 
         return "admin/index";
+    }
+
+    @RequestMapping("welcome.html")
+    public String weclome(ModelMap modelMap){
+
+        Map<String, String> map=System.getenv();
+        String userName = map.get("USERNAME");// 获取用户名
+        String computerName = map.get("COMPUTERNAME");// 获取计算机名
+        String userDomain = map.get("USERDOMAIN");// 获取计算机域名
+        modelMap.put("userName",userName);
+        modelMap.put("computerName",computerName);
+        modelMap.put("userDomain",userDomain);
+
+        return "admin/welcome";
     }
 
 
