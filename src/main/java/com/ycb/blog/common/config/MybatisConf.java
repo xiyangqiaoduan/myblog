161818140751d4055ed1918d1ea63a8792667237
yangcb <yangcb@hui10.com>
2017-05-19 18:18:56 +0800

    pagehelper使用

---
+++ src/main/java/com/ycb/blog/common/config/MybatisConf.java
@@ -0,0 +1,29 @@
+package com.ycb.blog.common.config;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * 注册MyBatis分页插件PageHelper
+ * @author yangcb
+ * @create 2017-05-19 17:36
+ **/
+@Configuration
+public class MybatisConf {
+
+    @Bean
+    public PageHelper pageHelper() {
+        System.out.println("MyBatisConfiguration.pageHelper()");
+        PageHelper pageHelper = new PageHelper();
+        Properties p = new Properties();
+        p.setProperty("offsetAsPageNum", "true");
+        p.setProperty("rowBoundsWithCount", "true");
+        p.setProperty("reasonable", "true");
+        pageHelper.setProperties(p);
+        return pageHelper;
+    }
+
+}
